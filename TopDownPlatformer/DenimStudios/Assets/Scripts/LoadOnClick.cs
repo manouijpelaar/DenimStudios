f5799f4b4e6a41727117abf9436239a6156f68e2
manouijpelaar <manou.ijpelaar@gmail.com>
2020-04-17 22:45:12 +0200

    SceneManagement completed

---
+++ TopDownPlatformer/DenimStudios/Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadOnClick : MonoBehaviour
+{
+    
+    // Loads scene if application is used (or pressed when set on a button).
+    public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
---
+++ TopDownPlatformer/DenimStudios/Assets/Scripts/winTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class winTrigger : MonoBehaviour
+{
+    public GameObject player;
+
+    //public GameObject completeLevelScreen;
+    //public TargetVolgorde targetManager;
+
+    void OnTriggerEnter(Collider win)
+    {   // when player collides with winscene, level complete
+        if (win.gameObject.tag == "Player")
+        {
+            player.GetComponent<characterController>().enabled = false;
+            SceneManager.LoadScene(3);
+            Debug.Log("You Win!");
+        }
+    }
+}
